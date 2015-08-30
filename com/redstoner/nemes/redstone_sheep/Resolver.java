9e0797015bf0ac105b8af0fa0b71a3ed099c2766
NEMESIS13cz <seke94@seznam.cz>
2015-08-30 02:52:20 +0200

    Progress

--- com/redstoner/nemes/redstone_sheep/Resolver.java
+++ com/redstoner/nemes/redstone_sheep/Resolver.java
@@ -3,6 +3,16 @@ package com.redstoner.nemes.redstone_sheep;
 public class Resolver {
 
 	public static void resolve(String text, String fileName) {
+		
+		
+		
 		FileManager.write(text, "1000101", fileName);
+	}
+	
+	public String encodeInstruction(String instr, String data1, String data2, String data3) {
+		
+		
+		
+		return "";
 	}
 }
