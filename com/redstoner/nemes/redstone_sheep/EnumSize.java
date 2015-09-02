8fcbe10eadeb9dbf3cb32c0c5b16d6a07c643121
NEMESIS13cz <seke94@seznam.cz>
2015-09-02 20:09:21 +0200

    Added more instructions

--- com/redstoner/nemes/redstone_sheep/EnumSize.java
+++ com/redstoner/nemes/redstone_sheep/EnumSize.java
@@ -2,6 +2,6 @@ package com.redstoner.nemes.redstone_sheep;
 
 public enum EnumSize {
 
-	BITSIZE_2, BITSIZE_8, BITSIZE_16;
+	BITSIZE_2, BITSIZE_4, BITSIZE_8, BITSIZE_16;
 	
 }
--- com/redstoner/nemes/redstone_sheep/Resolver.java
+++ com/redstoner/nemes/redstone_sheep/Resolver.java
@@ -97,14 +97,358 @@ public class Resolver {
 					}else{
 						throw new BuildException(Instruction.COMF.toString() + ": memory address invalid!");
 					}
+				}else if (first.equals(Instruction.DECF.toString())) {
+					int addr = translateRegister(arg1);
+					boolean register = addr > -1;
+					if (!register) addr += 1000;
+					if (addr != -1) {
+						if (isInRange(addr, EnumSize.BITSIZE_8)) {
+							String instr = "000011" + (register ? "0" : "1") + fixToBitSize(addr, EnumSize.BITSIZE_8) + (arg2.toLowerCase().equals("-u") ? "0" : "1");
+							compiled += instr + "\n";
+						}else{
+							throw new BuildException(Instruction.DECF.toString() + ": memory address exceeds maximum!");
+						}
+					}else{
+						throw new BuildException(Instruction.DECF.toString() + ": memory address invalid!");
+					}
+				}else if (first.equals(Instruction.DECFSZ.toString())) {
+					int addr = translateRegister(arg1);
+					boolean register = addr > -1;
+					if (!register) addr += 1000;
+					if (addr != -1) {
+						if (isInRange(addr, EnumSize.BITSIZE_8)) {
+							String instr = "001011" + (register ? "0" : "1") + fixToBitSize(addr, EnumSize.BITSIZE_8) + (arg2.toLowerCase().equals("-u") ? "0" : "1");
+							compiled += instr + "\n";
+						}else{
+							throw new BuildException(Instruction.DECFSZ.toString() + ": memory address exceeds maximum!");
+						}
+					}else{
+						throw new BuildException(Instruction.DECFSZ.toString() + ": memory address invalid!");
+					}
+				}else if (first.equals(Instruction.INCF.toString())) {
+					int addr = translateRegister(arg1);
+					boolean register = addr > -1;
+					if (!register) addr += 1000;
+					if (addr != -1) {
+						if (isInRange(addr, EnumSize.BITSIZE_8)) {
+							String instr = "001010" + (register ? "0" : "1") + fixToBitSize(addr, EnumSize.BITSIZE_8) + (arg2.toLowerCase().equals("-u") ? "0" : "1");
+							compiled += instr + "\n";
+						}else{
+							throw new BuildException(Instruction.INCF.toString() + ": memory address exceeds maximum!");
+						}
+					}else{
+						throw new BuildException(Instruction.INCF.toString() + ": memory address invalid!");
+					}
+				}else if (first.equals(Instruction.INCFSZ.toString())) {
+					int addr = translateRegister(arg1);
+					boolean register = addr > -1;
+					if (!register) addr += 1000;
+					if (addr != -1) {
+						if (isInRange(addr, EnumSize.BITSIZE_8)) {
+							String instr = "001111" + (register ? "0" : "1") + fixToBitSize(addr, EnumSize.BITSIZE_8) + (arg2.toLowerCase().equals("-u") ? "0" : "1");
+							compiled += instr + "\n";
+						}else{
+							throw new BuildException(Instruction.INCFSZ.toString() + ": memory address exceeds maximum!");
+						}
+					}else{
+						throw new BuildException(Instruction.INCFSZ.toString() + ": memory address invalid!");
+					}
+				}else if (first.equals(Instruction.IORWF.toString())) {
+					int addr = translateRegister(arg1);
+					boolean register = addr > -1;
+					if (!register) addr += 1000;
+					if (addr != -1) {
+						if (isInRange(addr, EnumSize.BITSIZE_8)) {
+							String instr = "000100" + (register ? "0" : "1") + fixToBitSize(addr, EnumSize.BITSIZE_8) + "0";
+							compiled += instr + "\n";
+						}else{
+							throw new BuildException(Instruction.IORWF.toString() + ": memory address exceeds maximum!");
+						}
+					}else{
+						throw new BuildException(Instruction.IORWF.toString() + ": memory address invalid!");
+					}
+				}else if (first.equals(Instruction.MOVF.toString())) {
+					int addr = translateRegister(arg1);
+					boolean register = addr > -1;
+					if (!register) addr += 1000;
+					if (addr != -1) {
+						if (isInRange(addr, EnumSize.BITSIZE_8)) {
+							String instr = "001000" + (register ? "0" : "1") + fixToBitSize(addr, EnumSize.BITSIZE_8) + "0";
+							compiled += instr + "\n";
+						}else{
+							throw new BuildException(Instruction.MOVF.toString() + ": memory address exceeds maximum!");
+						}
+					}else{
+						throw new BuildException(Instruction.MOVF.toString() + ": memory address invalid!");
+					}
+				}else if (first.equals(Instruction.MOVWF.toString())) {
+					int addr = translateRegister(arg1);
+					boolean register = addr > -1;
+					if (!register) addr += 1000;
+					if (addr != -1) {
+						if (isInRange(addr, EnumSize.BITSIZE_8)) {
+							String instr = "0000001" + fixToBitSize(addr, EnumSize.BITSIZE_8) + (register ? "0" : "1");
+							compiled += instr + "\n";
+						}else{
+							throw new BuildException(Instruction.MOVWF.toString() + ": memory address exceeds maximum!");
+						}
+					}else{
+						throw new BuildException(Instruction.MOVWF.toString() + ": memory address invalid!");
+					}
+				}else if (first.equals(Instruction.NOP.toString())) {
+					String instr = "0000000000000000";
+					compiled += instr + "\n";
+				}else if (first.equals(Instruction.RLF.toString())) {
+					int addr = translateRegister(arg1);
+					boolean register = addr > -1;
+					if (!register) addr += 1000;
+					if (addr != -1) {
+						if (isInRange(addr, EnumSize.BITSIZE_8)) {
+							String instr = "001101" + (register ? "0" : "1") + fixToBitSize(addr, EnumSize.BITSIZE_8) + "0";
+							compiled += instr + "\n";
+						}else{
+							throw new BuildException(Instruction.RLF.toString() + ": memory address exceeds maximum!");
+						}
+					}else{
+						throw new BuildException(Instruction.RLF.toString() + ": memory address invalid!");
+					}
+				}else if (first.equals(Instruction.RRF.toString())) {
+					int addr = translateRegister(arg1);
+					boolean register = addr > -1;
+					if (!register) addr += 1000;
+					if (addr != -1) {
+						if (isInRange(addr, EnumSize.BITSIZE_8)) {
+							String instr = "001100" + (register ? "0" : "1") + fixToBitSize(addr, EnumSize.BITSIZE_8) + "0";
+							compiled += instr + "\n";
+						}else{
+							throw new BuildException(Instruction.RRF.toString() + ": memory address exceeds maximum!");
+						}
+					}else{
+						throw new BuildException(Instruction.RRF.toString() + ": memory address invalid!");
+					}
+				}else if (first.equals(Instruction.SUBWF.toString())) {
+					int addr = translateRegister(arg1);
+					boolean register = addr > -1;
+					if (!register) addr += 1000;
+					if (addr != -1) {
+						if (isInRange(addr, EnumSize.BITSIZE_8)) {
+							String instr = "000010" + (register ? "0" : "1") + fixToBitSize(addr, EnumSize.BITSIZE_8) + (arg2.toLowerCase().equals("-u") ? "0" : "1");
+							compiled += instr + "\n";
+						}else{
+							throw new BuildException(Instruction.SUBWF.toString() + ": memory address exceeds maximum!");
+						}
+					}else{
+						throw new BuildException(Instruction.SUBWF.toString() + ": memory address invalid!");
+					}
+				}else if (first.equals(Instruction.SWAPF.toString())) {
+					int addr = translateRegister(arg1);
+					boolean register = addr > -1;
+					if (!register) addr += 1000;
+					if (addr != -1) {
+						if (isInRange(addr, EnumSize.BITSIZE_8)) {
+							String instr = "001110" + (register ? "0" : "1") + fixToBitSize(addr, EnumSize.BITSIZE_8) + "0";
+							compiled += instr + "\n";
+						}else{
+							throw new BuildException(Instruction.SWAPF.toString() + ": memory address exceeds maximum!");
+						}
+					}else{
+						throw new BuildException(Instruction.SWAPF.toString() + ": memory address invalid!");
+					}
+				}else if (first.equals(Instruction.XORWF.toString())) {
+					int addr = translateRegister(arg1);
+					boolean register = addr > -1;
+					if (!register) addr += 1000;
+					if (addr != -1) {
+						if (isInRange(addr, EnumSize.BITSIZE_8)) {
+							String instr = "000110" + (register ? "0" : "1") + fixToBitSize(addr, EnumSize.BITSIZE_8) + "0";
+							compiled += instr + "\n";
+						}else{
+							throw new BuildException(Instruction.XORWF.toString() + ": memory address exceeds maximum!");
+						}
+					}else{
+						throw new BuildException(Instruction.XORWF.toString() + ": memory address invalid!");
+					}
+				}else if (first.equals(Instruction.BCF.toString())) {
+					int addr = translateRegister(arg1);
+					boolean register = addr > -1;
+					if (!register) addr += 1000;
+					if (addr != -1 && !register) {
+						if (isInRange(addr, EnumSize.BITSIZE_8)) {
+							if (isNumber(arg2) && isInRange(Integer.parseInt(arg2), EnumSize.BITSIZE_4)) {
+								String instr = "0100" + fixToBitSize(Integer.parseInt(arg2), EnumSize.BITSIZE_4) + fixToBitSize(addr, EnumSize.BITSIZE_8);
+								compiled += instr + "\n";
+							}else{
+								throw new BuildException(Instruction.BCF.toString() + ": bit address invalid!");
+							}
+						}else{
+							throw new BuildException(Instruction.BCF.toString() + ": memory address exceeds maximum!");
+						}
+					}else{
+						throw new BuildException(Instruction.BCF.toString() + ": memory address invalid!");
+					}
+				}else if (first.equals(Instruction.BSF.toString())) {
+					int addr = translateRegister(arg1);
+					boolean register = addr > -1;
+					if (!register) addr += 1000;
+					if (addr != -1 && !register) {
+						if (isInRange(addr, EnumSize.BITSIZE_8)) {
+							if (isNumber(arg2) && isInRange(Integer.parseInt(arg2), EnumSize.BITSIZE_4)) {
+								String instr = "0101" + fixToBitSize(Integer.parseInt(arg2), EnumSize.BITSIZE_4) + fixToBitSize(addr, EnumSize.BITSIZE_8);
+								compiled += instr + "\n";
+							}else{
+								throw new BuildException(Instruction.BSF.toString() + ": bit address invalid!");
+							}
+						}else{
+							throw new BuildException(Instruction.BSF.toString() + ": memory address exceeds maximum!");
+						}
+					}else{
+						throw new BuildException(Instruction.BSF.toString() + ": memory address invalid!");
+					}
+				}else if (first.equals(Instruction.BTFSC.toString())) {
+					int addr = translateRegister(arg1);
+					boolean register = addr > -1;
+					if (!register) addr += 1000;
+					if (addr != -1 && !register) {
+						if (isInRange(addr, EnumSize.BITSIZE_8)) {
+							if (isNumber(arg2) && isInRange(Integer.parseInt(arg2), EnumSize.BITSIZE_4)) {
+								String instr = "0110" + fixToBitSize(Integer.parseInt(arg2), EnumSize.BITSIZE_4) + fixToBitSize(addr, EnumSize.BITSIZE_8);
+								compiled += instr + "\n";
+							}else{
+								throw new BuildException(Instruction.BTFSC.toString() + ": bit address invalid!");
+							}
+						}else{
+							throw new BuildException(Instruction.BTFSC.toString() + ": memory address exceeds maximum!");
+						}
+					}else{
+						throw new BuildException(Instruction.BTFSC.toString() + ": memory address invalid!");
+					}
+				}else if (first.equals(Instruction.BTFSS.toString())) {
+					int addr = translateRegister(arg1);
+					boolean register = addr > -1;
+					if (!register) addr += 1000;
+					if (addr != -1 && !register) {
+						if (isInRange(addr, EnumSize.BITSIZE_8)) {
+							if (isNumber(arg2) && isInRange(Integer.parseInt(arg2), EnumSize.BITSIZE_4)) {
+								String instr = "0111" + fixToBitSize(Integer.parseInt(arg2), EnumSize.BITSIZE_4) + fixToBitSize(addr, EnumSize.BITSIZE_8);
+								compiled += instr + "\n";
+							}else{
+								throw new BuildException(Instruction.BTFSS.toString() + ": bit address invalid!");
+							}
+						}else{
+							throw new BuildException(Instruction.BTFSS.toString() + ": memory address exceeds maximum!");
+						}
+					}else{
+						throw new BuildException(Instruction.BTFSS.toString() + ": memory address invalid!");
+					}
+				}else if (first.equals(Instruction.ADDLW.toString())) {
+					if (isNumber(arg1)) {
+						int lit = Integer.parseInt(arg1);
+						if (isInRange(lit, EnumSize.BITSIZE_8)) {
+							String instr = "111110" + fixToBitSize(lit, EnumSize.BITSIZE_8) + "0" + (arg2.toLowerCase().equals("-u") ? "0" : "1");
+							compiled += instr + "\n";
+						}else{
+							throw new BuildException(Instruction.ADDLW.toString() + ": literal too big!");
+						}
+					}else{
+						throw new BuildException(Instruction.ADDLW.toString() + ": literal invalid!");
+					}
+				}else if (first.equals(Instruction.ANDLW.toString())) {
+					if (isNumber(arg1)) {
+						int lit = Integer.parseInt(arg1);
+						if (isInRange(lit, EnumSize.BITSIZE_8)) {
+							String instr = "111001" + fixToBitSize(lit, EnumSize.BITSIZE_8) + "00";
+							compiled += instr + "\n";
+						}else{
+							throw new BuildException(Instruction.ANDLW.toString() + ": literal too big!");
+						}
+					}else{
+						throw new BuildException(Instruction.ANDLW.toString() + ": literal invalid!");
+					}
+				}else if (first.equals(Instruction.CALL.toString())) {
+					//TODO CALL
+				}else if (first.equals(Instruction.CLRWDT.toString())) {
+					String instr = "0000000110010000";
+					compiled += instr + "\n";
+				}else if (first.equals(Instruction.GOTO.toString())) {
+					//TODO GOTO
+				}else if (first.equals(Instruction.IORLW.toString())) {
+					if (isNumber(arg1)) {
+						int lit = Integer.parseInt(arg1);
+						if (isInRange(lit, EnumSize.BITSIZE_8)) {
+							String instr = "111000" + fixToBitSize(lit, EnumSize.BITSIZE_8) + "00";
+							compiled += instr + "\n";
+						}else{
+							throw new BuildException(Instruction.IORLW.toString() + ": literal too big!");
+						}
+					}else{
+						throw new BuildException(Instruction.IORLW.toString() + ": literal invalid!");
+					}
+				}else if (first.equals(Instruction.MOVLW.toString())) {
+					if (isNumber(arg1)) {
+						int lit = Integer.parseInt(arg1);
+						if (isInRange(lit, EnumSize.BITSIZE_8)) {
+							String instr = "110000" + fixToBitSize(lit, EnumSize.BITSIZE_8) + "00";
+							compiled += instr + "\n";
+						}else{
+							throw new BuildException(Instruction.MOVLW.toString() + ": literal too big!");
+						}
+					}else{
+						throw new BuildException(Instruction.MOVLW.toString() + ": literal invalid!");
+					}
+				}else if (first.equals(Instruction.RETFIE.toString())) {
+					String instr = "0000000000100100";
+					compiled += instr + "\n";
+				}else if (first.equals(Instruction.RETLW.toString())) {
+					if (isNumber(arg1)) {
+						int lit = Integer.parseInt(arg1);
+						if (isInRange(lit, EnumSize.BITSIZE_8)) {
+							String instr = "110100" + fixToBitSize(lit, EnumSize.BITSIZE_8) + "00";
+							compiled += instr + "\n";
+						}else{
+							throw new BuildException(Instruction.RETLW.toString() + ": literal too big!");
+						}
+					}else{
+						throw new BuildException(Instruction.RETLW.toString() + ": literal invalid!");
+					}
+				}else if (first.equals(Instruction.RETURN.toString())) {
+					String instr = "0000000000100000";
+					compiled += instr + "\n";
+				}else if (first.equals(Instruction.SLEEP.toString())) {
+					String instr = "0000000110001100";
+					compiled += instr + "\n";
+				}else if (first.equals(Instruction.SUBLW.toString())) {
+					if (isNumber(arg1)) {
+						int lit = Integer.parseInt(arg1);
+						if (isInRange(lit, EnumSize.BITSIZE_8)) {
+							String instr = "111100" + fixToBitSize(lit, EnumSize.BITSIZE_8) + "0" + (arg2.toLowerCase().equals("-u") ? "0" : "1");
+							compiled += instr + "\n";
+						}else{
+							throw new BuildException(Instruction.SUBLW.toString() + ": literal too big!");
+						}
+					}else{
+						throw new BuildException(Instruction.SUBLW.toString() + ": literal invalid!");
+					}
+				}else if (first.equals(Instruction.XORLW.toString())) {
+					if (isNumber(arg1)) {
+						int lit = Integer.parseInt(arg1);
+						if (isInRange(lit, EnumSize.BITSIZE_8)) {
+							String instr = "111010" + fixToBitSize(lit, EnumSize.BITSIZE_8) + "00";
+							compiled += instr + "\n";
+						}else{
+							throw new BuildException(Instruction.XORLW.toString() + ": literal too big!");
+						}
+					}else{
+						throw new BuildException(Instruction.XORLW.toString() + ": literal invalid!");
+					}
 				}
 			}catch (BuildException e) {
-				e.printStackTrace();
+				System.err.println(e.getLocalizedMessage());
 				return;
 			}
 			// </semi-dirty>
 		}
 		
+		System.out.println(compiled);
 		FileManager.write(text, compiled, fileName);
 	}
 	
@@ -124,6 +468,8 @@ public class Resolver {
 		}
 		if (range == EnumSize.BITSIZE_2 && i <= 0x3) {
 			return true;
+		}else if (range == EnumSize.BITSIZE_4 && i <= 0xF) {
+			return true;
 		}else if (range == EnumSize.BITSIZE_8 && i <= 0xFF) {
 			return true;
 		}else if (range == EnumSize.BITSIZE_16 && i <= 0xFFFF) {
@@ -137,6 +483,10 @@ public class Resolver {
 		int begin = s.length();
 		if (range == EnumSize.BITSIZE_2 && s.length() < 2) {
 			for (int i = 0; i < 2 - begin; i++) {
+				s = '0' + s;
+			}
+		}else if (range == EnumSize.BITSIZE_4 && s.length() < 4) {
+			for (int i = 0; i < 4 - begin; i++) {
 				s = '0' + s;
 			}
 		}else if (range == EnumSize.BITSIZE_8 && s.length() < 8) {
