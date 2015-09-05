a01492b22939edca00f9b6124d6654b279edef5e
NEMESIS13cz <seke94@seznam.cz>
2015-09-05 16:52:53 +0200

    Mode now saves and loads to/from sheep_assembler.config

--- com/redstoner/nemes/redstone_sheep/Assembler.java
+++ com/redstoner/nemes/redstone_sheep/Assembler.java
@@ -5,7 +5,14 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
@@ -21,18 +28,31 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class Assembler {
 	
-	private static boolean darkMode = false;
+	private static boolean darkMode;
 	
 	public Assembler() {
 		JFrame frame = new JFrame();
 		JPanel panel = new JPanel();
 		frame.setTitle("Redstone Sheep Assembler");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		frame.setPreferredSize(new Dimension(600, 400));
 		frame.pack();
 		frame.setLocationRelativeTo(null);
+		WindowListener windowListener = new WindowListener() {
+
+			public void windowOpened(WindowEvent e) {}
+			public void windowClosing(WindowEvent e) {
+				saveConfig();
+			}
+			public void windowClosed(WindowEvent e) {}
+			public void windowIconified(WindowEvent e) {}
+			public void windowDeiconified(WindowEvent e) {}
+			public void windowActivated(WindowEvent e) {}
+			public void windowDeactivated(WindowEvent e) {}
+		};
 		
 		frame.add(panel);
+		frame.addWindowListener(windowListener);
 		
 		BoxLayout layout = new BoxLayout(panel, BoxLayout.Y_AXIS);
 		panel.setLayout(layout);
@@ -111,12 +131,51 @@ public class Assembler {
 		}
 	}
 	
+	public static void saveConfig() {
+		Thread t = new Thread() {
+			
+			public void run() {
+				try {
+					BufferedWriter writer = new BufferedWriter(new FileWriter(new File("sheep_assembler.config")));
+					writer.write("dark=" + String.valueOf(darkMode));
+					writer.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				System.exit(0);
+			}
+		};
+		t.start();
+	}
+	
+	public static void loadConfig() {
+		File f = new File("sheep_assembler.config");
+		if (!f.exists()) {
+			darkMode = false;
+			return;
+		}
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(f));
+			String buffer = "";
+			while ((buffer = reader.readLine()) != null) {
+				if (buffer.startsWith("dark")) {
+					String value = buffer.substring(buffer.indexOf('=') + 1, buffer.length());
+					darkMode = Boolean.parseBoolean(value);
+				}
+			}
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	public static void main(String[] args) {
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
 			e.printStackTrace();
 		}
+		loadConfig();
 		new Assembler();
 	}
 }
